9f40e2b75969cdf16d65a80fbb4cac0e89b9b022
krasnobcin <82445247+krasnobcin@users.noreply.github.com>
2026-04-01 21:15:46 +0300

    Delete lab2/lab2/src/store/actions.js

--- lab2/lab2/src/store/actions.js
+++
@@ -1,6 +0,0 @@
-export const actions = {
-  search: (value) => ({ type: "SEARCH", payload: value }),
-  addToCart: (product) => ({ type: "ADD_TO_CART", payload: product }),
-  removeFromCart: (id) => ({ type: "REMOVE_FROM_CART", payload: id }),
-  clearCart: () => ({ type: "CLEAR_CART" })
-};
